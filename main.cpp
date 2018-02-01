f719baa5f7b239e31856cfd59efa08a29ccd654a
CalebSerafin <31820984+CalebSerafin@users.noreply.github.com>
2018-02-01 23:40:28 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 31 Jan 2018
+ *      Author: Caleb Serafin
+ */
+
+#include <iostream>
+
+int main()
+{
+	for(int i=2;i<=100;i+=2)
+	{
+		std::cout<<i<<std::endl;
+	};
+	return 0;
+};
